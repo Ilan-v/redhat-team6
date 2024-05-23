058d34d6f43b0b9739a33c9bf9baf39a4e3279df
unknown <dunskyshahar@gmail.com>
2024-05-23 05:02:13 +0300

    add create help suggestion

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import RegisterView from '../views/RegisterView.vue'
+import CreateHelpSuggestion from '../views/CreateHelpSuggestion.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/register',
-      name: 'registration',
-      component: RegisterView
+      name: 'createHelpSuggestion',
+      component: CreateHelpSuggestion
     }
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,3 +8,20 @@ import Button from "primevue/button"
     <Button label="דךלכחשד"/>
   </main>
 </template>
+
+<template #option="slotProps">
+  <i :class="slotProps.option.icon" style="font-size: 1.5rem; width: 3.6rem"></i>
+
+<div class="flex pt-4 justify-between">
+<Button
+  label="אחורה"
+  severity="secondary"
+  icon="pi pi-arrow-right"
+  @click="emit('prevCallback')"
+/>
+<Button label="הבא" icon="pi pi-arrow-left" iconPos="right" @click="emit('nextCallback')" />
+</div>
+</div>
+</template>
+
+
